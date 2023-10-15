33f805d386e17a3b63636eb4308850010132da4f
Dzmitry <dmitry.litmanovich@pm.me>
2023-10-15 05:21:46 +0400

    api/feature/users-services: add jest.

--- providers/pg-performer.js
+++ providers/pg-performer.js
@@ -4,6 +4,7 @@ export default async function performDatabaseOperation(query, values) {
   try {
     const result = await db.query(query, values);
     console.log(result.rows);
+    
     return result;
   } catch (error) {
     console.error('Error performing database operation:', error);
